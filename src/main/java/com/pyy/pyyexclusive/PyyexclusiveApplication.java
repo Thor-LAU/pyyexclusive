7c55033fc5f86e7699c2fcb5e9b345f445ed01b6
Thor-LAU <ThorLau990104@gmail.com>
2021-08-25 15:13:24 +0800

    Pyy

---
+++ src/main/java/com/pyy/pyyexclusive/PyyexclusiveApplication.java
@@ -0,0 +1,13 @@
+package com.pyy.pyyexclusive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyyexclusiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyyexclusiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pyy/pyyexclusive/PyyexclusiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyy.pyyexclusive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyyexclusiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
